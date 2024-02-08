dcb6f29f3e8bfcbb0a96662a82a2939f269458e0
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2024-02-08 16:15:48 +0530

    Stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { Routes } from 'react-router'
 import './App.css'
+import './styles.scss';
 
 function App() {
 
   return (
-    <div id='app'>
-      <Routes>
-
-      </Routes>
+    <div id='app' className='w-full h-auto bg-dark text-danger'>
+      Welcome
+      <Routes></Routes>
     </div>
   )
 }
