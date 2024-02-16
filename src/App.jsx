6da64e3f7f41e8dd54b28bed342a5dccc3ae1282
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2024-02-16 14:56:46 +0530

    [Majors] ~ Postcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from './assets/vite.svg'
 import './App.css'
 
 function App() {
@@ -32,4 +32,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
