f99825a36e42e881ecff2bb760d159c35d6d5a77
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2024-02-17 12:52:57 +0530

    Configurations

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import './App.css'
+import './styles.scss';
 
 function App() {
   const [count, setCount] = useState(0)
